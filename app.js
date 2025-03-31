d6c48da5a692d5317ed8709fb15337a84ce1be9f
Sandesh Gadal <gadalsandesh123@gmail.com>
2025-03-31 23:31:15 +0545

    routing and the dynamic rendering in ejs

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const app = express()  // storing it in the app variable we will use it througho
 app.set("view engine","ejs") // setting the view engine to ejs
 
 app.get('/',(req,res)=>{
-    console.log(req)
-    res.send("<h1>this is the Node app home page for the </h1>") // this is the response we will get when we visit the localhost:3000
+  const person = {
+    name: "Sandesh gadal"  ,
+    age: 22,
+    country: "Nepal",
+    hobbies: ["coding","reading","gaming"],
+    address: {
+        city: "Chitwan",
+        state: "Bagmati"
+    },
+  }
+    res.render("home", {person}) // this is the response we will get when we visit the localhost:3000
 
 })
 
 app.get('/about',(req,res)=>{
-    res.send('About Page') // this is the response we will get when we visit the localhost:3000/about
+    res.render("about") // this is the response we will get when we visit the localhost:3000/about
 
 })
 
